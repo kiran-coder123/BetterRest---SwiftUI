850ed7b6b58440e6861349a4ab4bbb12b3654aff
kiran sonne <kiransonne04@gmail.com>
2022-11-21 20:45:13 +0530

    Initial Commit

---
+++ SleepCalculatorApp/SleepCalculatorAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SleepCalculatorAppApp.swift
+//  SleepCalculatorApp
+//
+//  Created by Kiran Sonne on 21/11/22.
+//
+
+import SwiftUI
+
+@main
+struct SleepCalculatorAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
